cf227db7d7f373529b3c81ebeb28174667045528
Muhammad Huzaifa <154605966+MuhammadHuzaifa9@users.noreply.github.com>
2024-06-07 14:01:17 +0500

    Add files via upload

---
+++ basicapp/app.js
@@ -0,0 +1,15 @@
+let heading=document.getElementById('materialhead');
+let mtext=document.getElementById('materialtext');
+function changeHeadingVision(){
+    heading.innerHTML=`Vision`;
+    mtext.innerHTML=`Man bun PBR&B keytar copper mug prism, hell of helvetica. Synth crucifix offal deep v hella biodiesel. Church-key listicle polaroid put a bird on it chillwave palo santo enamel pin, tattooed meggings franzen la croix cray. Retro yr aesthetic four loko tbh helvetica air plant, neutra palo santo tofu mumblecore. Hoodie bushwick pour-over jean shorts chartreuse shabby chic. Roof party hammock master cleanse pop-up truffaut, bicycle rights skateboard affogato readymade sustainable deep v live-edge schlitz narwhal.<ul><li>List item</li><li>List item</li><li>List item</li></ul>`
+}
+function changeHeadingHistory(){
+    heading.innerHTML=`History`;
+    mtext.innerHTML=`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, praesentium vel fugit distinctio commodi ipsam tempore amet itaque nostrum doloremque quae optio, tempora suscipit esse eos eaque. Reprehenderit, qui soluta.lo Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, magni totam? Corporis molestias corrupti illum explicabo, quod nostrum voluptatum accusantium. Consectetur molestias modi incidunt cupiditate repellendus nesciunt quia nostrum similique!`
+}
+function changeHeadingGoals(){
+    heading.innerHTML=`Goals`;
+    mtext.innerHTML=`Chambray authentic truffaut, kickstarter brunch taxidermy vape heirloom four dollar toast raclette shoreditch church-key. Poutine etsy tote bag, cred fingerstache leggings cornhole everyday carry blog gastropub. Brunch biodiesel sartorial mlkshk swag, mixtape hashtag marfa readymade direct trade man braid cold-pressed roof party. Small batch adaptogen coloring book heirloom. Letterpress food truck hammock literally hell of wolf beard adaptogen everyday carry. Dreamcatcher pitchfork yuccie, banh mi salvia venmo photo booth quinoa chicharrones.`
+}
+
