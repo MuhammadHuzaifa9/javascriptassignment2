7f84610a55a22663f0d2c8a6502bd52b8249b2a6
Muhammad Huzaifa <154605966+MuhammadHuzaifa9@users.noreply.github.com>
2024-07-28 21:09:24 +0500

    Add files via upload

--- githubSearch/app.js
+++ githubSearch/app.js
@@ -1,13 +1,22 @@
 function fetchUser(){
     const box=document.querySelector('.box');
     const user=document.getElementById('search').value;
+    if(user.trim()==''){
+        box.innerHTML=`<p>Please Enter A Username!</p>`
+    }
+    else{
     fetch(`https://api.github.com/users/${user}`)
     .then((res)=>{
+        if (res.status==404){
+            box.innerHTML=`<p>User Not Found!</p>`;
+            throw new Error('User not found!');
+        }
+        else{
         return res.json();
+    }
     })
     .then((data)=>{
-            box.innerHTML=``
-           box.innerHTML +=`
+           box.innerHTML =`
                 <div class="col-md-6 container">
                 <div class="mt-4 card" style="border:none;" >
                     <img src="${data.avatar_url}" class="card-img-top avimg" alt="...">
@@ -16,7 +25,7 @@ function fetchUser(){
                         <h5 class="card-title">${data.login}</h5>
                         <p class="card-text">${data.bio ? data.bio : 'No bio available'}</p>
                         <p class="card-text followers"><i class="fa-solid fa-person"></i> Followers: ${data.followers}<br><i class="fa-solid fa-person"></i> Following: ${data.following}</p>
-                        <p class="card-text"><i class="fa-solid fa-location"></i>${data.location}</p>
+                        <p class="card-text"><i class="fa-solid fa-location"></i>${data.location ? data.location : 'Unknown'}</p>
                         <a href="${data.html_url}" class="w-100 btn btn-primary">Follow</a>
                     </div>
                 </div></div>`
@@ -27,4 +36,5 @@ function fetchUser(){
     }
 
     )
+}
 }
